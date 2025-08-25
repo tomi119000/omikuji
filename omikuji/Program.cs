aa1b2b63a958dc1cf18a134ee226a5d164cc046f
tomi119000 <135408771+tomi119000@users.noreply.github.com>
2025-08-25 13:48:13 +0900

    8/25 4限終了時点

--- omikuji/Program.cs
+++ omikuji/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+string[] omikuji = new string[5];
+
+omikuji[0] = "0大吉";
+omikuji[1] = "1中吉";
+omikuji[2] = "2吉";
+omikuji[3] = "3凶";
+omikuji[4] = "4大凶";
+
+//string[] omikuji = {"0大吉", "1中吉", "2吉", "3凶", "4大凶"};
+
+//各おみくじを何回引いたかを格納する配列
+int[] count = new int[5];
+
+// RandomクラスのNext命令を呼び出し、インスタンスを作成して乱数を取得
+Random rand = new Random();
+int result = rand.Next(0, 5); //0以上5未満の数をランダムに生成してresultに代入
+
+// resultの値を配列かひっぱってきてコンソールに出す
+Console.WriteLine(omikuji[result]);
